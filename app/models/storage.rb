745a80a1ce1678b289fec6ffe4b6f9c16693108b
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-20 17:51:48 +0000

    added pic upload

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,6 +1,7 @@
 class Storage < ApplicationRecord
 
   has_many :bookings, dependent: :destroy
+  has_attachment :photo
 
   validates :title, presence: true, uniqueness: true
   validates :size, presence: true
