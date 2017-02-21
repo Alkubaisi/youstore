a107eba7ac85271234b193751b21746edeee0713
Harry Bennett <harry.bennett1@gmail.com>
2017-02-21 17:06:33 +0000

    bookings controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221142320) do
+ActiveRecord::Schema.define(version: 20170221164442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,10 +33,10 @@ ActiveRecord::Schema.define(version: 20170221142320) do
   create_table "bookings", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "storage_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "time_wanted"
-    t.boolean  "confirmed"
+    t.string   "status",      default: "pending"
     t.index ["storage_id"], name: "index_bookings_on_storage_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
@@ -69,14 +69,16 @@ ActiveRecord::Schema.define(version: 20170221142320) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "bookings", "storages"
   add_foreign_key "bookings", "users"
-<<<<<<< HEAD
-=======
   add_foreign_key "storages", "users"
->>>>>>> master
 end
