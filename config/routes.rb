ff39bd49f42aec160b8acf5b6ac8a08f2e9357ce
Harry Bennett <harry.bennett1@gmail.com>
2017-02-21 14:37:40 +0000

    add fb connect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :storages
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'storages#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
