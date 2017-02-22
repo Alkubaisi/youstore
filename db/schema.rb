c027d71d306ecf13ce41859b59be40278e122a88
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-22 15:43:33 +0000

    added results

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222123149) do
+ActiveRecord::Schema.define(version: 20170222144214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20170222123149) do
     t.integer  "price"
     t.index ["storage_id"], name: "index_bookings_on_storage_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "results", force: :cascade do |t|
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "storages_id"
+    t.index ["storages_id"], name: "index_results_on_storages_id", using: :btree
   end
 
   create_table "storages", force: :cascade do |t|
