f117d21129035d4ef5eaa42a29097fcdeecbb80f
Kit Tjia <wktjia@gmail.com>
2017-02-22 14:15:08 +0000

    merge with master

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'storages#index'
   get "/about", to: "pages#about"
+  get "/resultspage", to: "pages#resultspage"
   get "/profile", to: "pages#profile"
+
 end
