6bd9a2aeffee33d8be4767c158c163ddbd5797e5
Kit Tjia <wktjia@gmail.com>
2017-02-22 12:44:21 +0000

    booking form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221164442) do
+ActiveRecord::Schema.define(version: 20170222123149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20170221164442) do
     t.datetime "updated_at",                      null: false
     t.string   "time_wanted"
     t.string   "status",      default: "pending"
+    t.date     "from"
+    t.date     "to"
+    t.integer  "price"
     t.index ["storage_id"], name: "index_bookings_on_storage_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
