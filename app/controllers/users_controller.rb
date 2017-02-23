fbe92a5a93d517ccacf84047d868118d25a8a602
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-23 14:21:26 +0000

    updated dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
- before_filter :authenticate_user!
   def show
     # @user = User.find(params[:id])
    end
@@ -8,25 +7,6 @@ def dashboard
 
 end
 
-
-  def edit_profile
-    @user = current_user
-  end
-
-  def update_profile
-    @user = User.find(current_user.id)
-    if @user.update(user_params)
-      redirect_to root_path
-    else
-      render "edit"
-    end
-  end
-
-  private
-
-  def user_params
-    params.required(:user).permit(:xxx, :yyy)
-  end
 
 
 end
